10359d42057d4f199642380bddb05f86d5227671
PC <bright_1@ukr.net>
2025-01-10 11:47:07 +0200

    update create folder if not exists

--- app/api/v1/file.py
+++ app/api/v1/file.py
@@ -24,7 +24,10 @@ async def download_file(path: pathlib.Path = Depends(syspath)):
 async def upload_file(path: pathlib.Path = Depends(syspath), file: UploadFile = File(...)):
     """upload file to specified folder"""
     if not path.is_dir():
-        raise HTTPException(status_code=404)
+        try:
+            path.mkdir(parents=True, exist_ok=True)
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=f"Failed to create directory: {str(e)}")
     if not file.filename:
         raise HTTPException(status_code=422, detail="Name cannot be empty")
     new_file = path / pathlib.Path(file.filename)
@@ -34,6 +37,8 @@ async def upload_file(path: pathlib.Path = Depends(syspath), file: UploadFile = 
         raise HTTPException(status_code=422, detail=r"Name cannot contain \/:*?<>|")
     content = await file.read()
     await write(content, new_file)
+
+    #todo return path
     return SysFile(
         name=new_file.name,
         mime=get_mime(new_file),
