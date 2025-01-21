ec5202f7033c6be5d4f31068cafde977bbc6d238
PC <bright_1@ukr.net>
2025-01-21 08:52:35 +0200

    fix trouble with path in docker

--- app/main.py
+++ app/main.py
@@ -108,6 +108,6 @@ app.add_middleware(
 )
 
 
-# if __name__ == "__main__":
-#     import uvicorn
-#     uvicorn.run("main:app", host="0.0.0.0", port=5001, reload=True)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=5001, reload=True)
